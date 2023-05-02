fc783ceddbffa0c7bf87076dac6608595d88ba59
Nazzmus sakib <nazzmussakib990@gmail.com>
2023-05-02 14:24:09 +0600

    added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 2500;
+const cors = require("cors");
+const category = require("./data/category.json");
+const news = require("./data/news.json");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("server is running successful");
+});
+app.get("/category", (req, res) => {
+  res.send(category);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+app.get("/category/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  }
+  const filteredItem = news.filter((n) => parseInt(n.category_id) === id);
+  res.send(filteredItem);
+});
+// app.get("/category/:id", (req, res) => {
+//   const id = parseInt(req.params.id);
+//   if (id === 0) {
+//     res.send(news);
+//   } else {
+//     const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+//     res.send(categoryNews);
+//   }
+// });
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
